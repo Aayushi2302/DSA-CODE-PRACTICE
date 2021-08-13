df58fe5fc30dee572e4870eaf851539275e84f1f
Aayushi2302 <sharmaaayushi2302@gmail.com>
2021-08-13 12:50:39 +0530

    2nd commit

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 
 int main(){
-cout<<"Hello World";
-return 0;
+	cout<<"Hello World";
+	return 0;
 }
---
+++ sum_two_int.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int a,b;
+	cout<<"Enter the value of A and B :"<<endl;
+	cin>>a>>b;
+	cout<<"The sum of two integers "<<a<<" and "<<b<<" is : "<<(a+b);
+	return 0;
+}
